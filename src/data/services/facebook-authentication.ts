3cc09cddd90f27b87387451c552e879d789997dc
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-09-26 20:56:39 -0300

    feat: ensure UpdateFacebookAccountRepo creates new Account if LoadUserAccountRepo returns data

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,19 +1,27 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
-import { CreateFacebookAccountRepository, LoadUserAccountRepository } from '../contracts/repos'
+import { CreateFacebookAccountRepository, LoadUserAccountRepository, UpdateFacebookAccountRepository } from '../contracts/repos'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly userAccountRepo: LoadUserAccountRepository & CreateFacebookAccountRepository
+    private readonly userAccountRepo: LoadUserAccountRepository & CreateFacebookAccountRepository & UpdateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const facebookData = await this.facebookApi.loadUser(params)
     if (facebookData !== undefined) {
-      await this.userAccountRepo.load({ email: facebookData.email })
-      await this.userAccountRepo.createFromFacebook(facebookData)
+      const accountData = await this.userAccountRepo.load({ email: facebookData.email })
+      if (accountData?.name !== undefined) {
+        await this.userAccountRepo.updateWithFacebook({
+          id: accountData.id,
+          name: accountData.name,
+          facebookId: facebookData.facebookId
+        })
+      } else {
+        await this.userAccountRepo.createFromFacebook(facebookData)
+      }
     }
     return new AuthenticationError()
   }
