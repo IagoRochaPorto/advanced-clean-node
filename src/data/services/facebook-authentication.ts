fa027dd1958ce828fe98a4201a907b8c557293ed
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-09-26 22:08:16 -0300

    feat: ensure FacebookAuthenticationService return an AccessToken on success

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -5,20 +5,21 @@ import { SaveFromFacebookAccountRepository, LoadUserAccountRepository } from '..
 import { AccessToken, FacebookAccount } from '@/domain/models'
 import { TokenGenerator } from '@/data/contracts/crypto'
 
-export class FacebookAuthenticationService {
+export class FacebookAuthenticationService implements FacebookAuthentication {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
     private readonly crypto: TokenGenerator,
     private readonly userAccountRepo: LoadUserAccountRepository & SaveFromFacebookAccountRepository
   ) {}
 
-  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
+  async perform (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
     const facebookData = await this.facebookApi.loadUser(params)
     if (facebookData !== undefined) {
       const accountData = await this.userAccountRepo.load({ email: facebookData.email })
       const facebookAccount = new FacebookAccount(facebookData, accountData)
       const { id } = await this.userAccountRepo.saveWithFacebook(facebookAccount)
-      await this.crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
+      const token = await this.crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
+      return new AccessToken(token)
     }
     return new AuthenticationError()
   }
