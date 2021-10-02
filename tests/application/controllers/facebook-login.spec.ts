948b36e95f8ffa0cd023c8afeb5a8887bc1f36ac
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-10-01 23:45:08 -0300

    test: change FacebookLoginController tests too mock RequiredStringValidator instead of test implementation

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,59 +1,56 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
-import { RequiredFieldError, ServerError, UnauthorizedError } from '@/application/errors'
+import { ServerError, UnauthorizedError } from '@/application/errors'
 import { FacebookLoginController } from '@/application/controllers/facebook-login'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+import { RequiredStringValidator } from '@/application/validation'
+import { mocked } from 'ts-jest/utils'
+
+jest.mock('@/application/validation/required-string')
 
 describe('FacebookLoginController', () => {
   let sut: FacebookLoginController
   let facebookAuth: MockProxy<FacebookAuthentication>
+  let token: string
 
   beforeAll(() => {
     facebookAuth = mock()
     facebookAuth.perform.mockResolvedValue(new AccessToken('any_value'))
+    token = 'any_token'
   })
   beforeEach(() => {
     sut = new FacebookLoginController(facebookAuth)
   })
 
-  it('Should return 400 if token is empty', async () => {
+  it('Should return 400 if validation fails', async () => {
+    const error = new Error('validation_error')
+    const RequiredStringValidatorSpy = jest.fn().mockImplementationOnce(() => ({
+      validate: jest.fn().mockReturnValueOnce(error)
+    }))
+    mocked(RequiredStringValidator).mockImplementation(RequiredStringValidatorSpy)
     const sut = new FacebookLoginController(facebookAuth)
 
-    const httpResponse = await sut.handle({ token: '' })
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new RequiredFieldError('token')
-    })
-  })
+    const httpResponse = await sut.handle({ token })
 
-  it('Should return 400 if token is null', async () => {
-    const httpResponse = await sut.handle({ token: null as any })
+    expect(RequiredStringValidatorSpy).toHaveBeenCalledWith('any_token', 'token')
     expect(httpResponse).toEqual({
       statusCode: 400,
-      data: new RequiredFieldError('token')
-    })
-  })
-
-  it('Should return 400 if token is undefined', async () => {
-    const httpResponse = await sut.handle({ token: undefined as any })
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new RequiredFieldError('token')
+      data: error
     })
   })
 
   it('Should call FacebookAuthentication with correct params', async () => {
-    await sut.handle({ token: 'any_token' })
-    expect(facebookAuth.perform).toHaveBeenCalledWith({ token: 'any_token' })
+    await sut.handle({ token })
+    expect(facebookAuth.perform).toHaveBeenCalledWith({ token })
     expect(facebookAuth.perform).toHaveBeenCalledTimes(1)
   })
 
   it('Should return 401 if authentication fails', async () => {
     facebookAuth.perform.mockResolvedValueOnce(new AuthenticationError())
 
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
     expect(httpResponse).toEqual({
       statusCode: 401,
       data: new UnauthorizedError()
@@ -61,7 +58,7 @@ describe('FacebookLoginController', () => {
   })
 
   it('Should return 200 if authentication succeeds', async () => {
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
     expect(httpResponse).toEqual({
       statusCode: 200,
       data: {
@@ -74,7 +71,7 @@ describe('FacebookLoginController', () => {
     const error = new Error('infra_error')
     facebookAuth.perform.mockRejectedValueOnce(error)
 
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
     expect(httpResponse).toEqual({
       statusCode: 500,
       data: new ServerError(error)
