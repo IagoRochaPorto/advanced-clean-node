d97c70125b6c609e1e919dfd97256f65bdd235a0
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-10-04 21:11:43 -0300

    refactor: move ExpressRouter to its own file

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -2,17 +2,7 @@ import { Request, Response } from 'express'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { mock, MockProxy } from 'jest-mock-extended'
 import { Controller } from 'application/controllers'
-
-class ExpressRouter {
-  constructor (
-    private readonly controller: Controller
-  ) {}
-
-  async adapt (req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body })
-    res.status(200).json(httpResponse.data)
-  }
-}
+import { ExpressRouter } from '@/infra/http'
 
 describe('ExpressRouter', () => {
   let req: Request
@@ -53,6 +43,34 @@ describe('ExpressRouter', () => {
     expect(res.status).toHaveBeenCalledTimes(1)
 
     expect(res.json).toHaveBeenCalledWith({ data: 'any_data' })
+    expect(res.json).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should respond with 400 and valid error', async () => {
+    controller.handle.mockResolvedValueOnce({
+      statusCode: 400,
+      data: new Error('any_error')
+    })
+
+    await sut.adapt(req, res)
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.status).toHaveBeenCalledTimes(1)
+
+    expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
+    expect(res.json).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should respond with 500 and valid error', async () => {
+    controller.handle.mockResolvedValueOnce({
+      statusCode: 500,
+      data: new Error('any_error')
+    })
+
+    await sut.adapt(req, res)
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+
+    expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
     expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
